8d4eba8c6e4d6783d776bcf2d193600091cac486
TheFinnigan <nathanpegs@gmail.com>
2024-05-13 22:33:10 -0500

    Enemy base
    enemies will path to the player and spawn from the void

--- void.gd
+++ void.gd
@@ -8,4 +8,5 @@ func _physics_process(delta):
 	if randi_range(0, 100) < 1:
 		var new_e = enemy.instantiate()
 		get_tree().current_scene.add_child(new_e)
-		new_e.position += Vector2(randf_range(-1.0,1.0), randf_range(-1.0,1.0))
+		new_e.position = Vector2(randf_range(-10.0,10.0), randf_range(-10.0,10.0))
+		print(new_e.position)
