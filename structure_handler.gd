418c404102a084d4e246b29fa5828ee0b8e72d32
PaladinWho <alexanderhtg@gmail.com>
2024-05-13 23:51:24 -0500

    Added a Tilemap getter for the Structure Handler
    the Structure handler is now able to reference the tilemap with the tile_map variable as well as get the mouse coords on the tilemap each click

--- structure_handler.gd
+++ structure_handler.gd
@@ -3,11 +3,14 @@ extends Control
 @onready var structure_but_1 = $VBoxContainer/buttonHolder/HBoxContainer/structureBut1 as Button
 @onready var structure_but_2 = $VBoxContainer/buttonHolder/HBoxContainer/structureBut2 as Button
 @onready var structure_but_3 = $VBoxContainer/buttonHolder/HBoxContainer/structureBut3 as Button
-var structure_selected = 0
+@onready var tile_map = get_node(self.get_meta("activeTilemap")) #Gets and sets a reference to the currently used tilemap
 
+var structure_selected = 0
+var clicked_cell
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(tile_map)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,5 +31,11 @@ func onButtonPressed(butNum):
 func _input(event):
 	if event is InputEventMouseButton:
 		if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT):
+			clicked_cell = tile_map.local_to_map(tile_map.get_local_mouse_position())
+			print(clicked_cell)
 			print("left click!")
+	if event is InputEventKey:
+		if (Input.is_key_pressed(KEY_SPACE) and not event.is_echo()):
+			structure_selected = 0
+			print("Structure Selected: ", structure_selected)
 	pass
