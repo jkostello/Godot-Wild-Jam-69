638844b751ecfc1c77ba24314f869af56f5e0c0c
TheFinnigan <nathanpegs@gmail.com>
2024-05-16 18:16:32 -0500

    done

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 # Player movement
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	direction.y *= 0.5
 	direction = direction.normalized()
 	velocity = direction * speed
 	velocity = velocity.move_toward(Vector2.ZERO, speed / 4)
