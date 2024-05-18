6ced32ec328d2c940a48be29df449d1266d79ef6
TheFinnigan <nathanpegs@gmail.com>
2024-05-18 14:41:10 -0500

    Update level.gd

--- level.gd
+++ level.gd
@@ -6,8 +6,9 @@ var ending := false
 func _physics_process(delta):
 	if not get_tree().get_nodes_in_group("Laser") and not ending:
 		end_game()
-	
-# TODO: finish function
+
+
+
 func end_game():
 	ending = true
 	$AnimationPlayer.play('die')
