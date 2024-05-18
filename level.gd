b2eb75505459c2b42bea6076f4313010422304a6
TheFinnigan <nathanpegs@gmail.com>
2024-05-18 14:29:01 -0500

    game end anim+sound

--- level.gd
+++ level.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+var ending := false
 
 # End game if all lasers die
 func _physics_process(delta):
-	if not get_tree().get_nodes_in_group("Laser"):
+	if not get_tree().get_nodes_in_group("Laser") and not ending:
 		end_game()
 	
 # TODO: finish function
 func end_game():
-	pass
+	ending = true
+	$AnimationPlayer.play('die')
+	$AudioStreamPlayer.play()
+	
